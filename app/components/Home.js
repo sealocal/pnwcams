1ba3d360997d37bc7d2607e90a927e115faaa02c
Brian Behrens <brian@brainbrian.com>
2015-07-06 22:15:31 -0700

    Set up basic routing with React-Router

---
+++ app/components/Home.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Home = React.createClass({
+  render: function() {
+    return (
+      <h2 className="home-content">
+        Snow and surf web cameras from the Pacific Northwest.
+      </h2>
+    )
+  }
+});
+
+module.exports = Home;
--- app/components/Main.js
+++ app/components/Main.js
@@ -1,13 +1,38 @@
 var React = require('react');
+var RouteHandler = require('react-router').RouteHandler;
 
 var Main = React.createClass({
   render: function() {
     return (
-      <div>
-        PNW Cams!
+      <div className="main-container">
+        <div className="container">
+
+          <nav className="navbar navbar-default" role="navigation">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                  <span className="sr-only">Toggle navigation</span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="#">PNW Cams</a>
+              </div>
+              <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                  <li><a href="#/surf">Surf</a></li>
+                  <li><a href="#/snow">Snow</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <div className="container">
+          <RouteHandler />
+        </div>
       </div>
     )
   }
 });
 
-React.render(<Main />, document.getElementById('app'));
+module.exports = Main;
