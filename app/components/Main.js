882063d8804bf3c18d5c542e0faf078222732fa7
Brian Behrens <brian@brainbrian.com>
2015-07-06 21:09:02 -0700

    Update app structure to support basic ReactJS and Webpack

---
+++ app/components/Main.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <div>
+        PNW Cams!
+      </div>
+    )
+  }
+});
+
+React.render(<Main />, document.getElementById('app'));
